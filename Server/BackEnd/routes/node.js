5d7a6686e48f19f5118114b68d0be3da9d40420f
Dinusha Naveen <dldndasanayaka@gmail.com>
2019-03-21 22:59:37 +0530

    edits

--- Server/BackEnd/routes/node.js
+++ Server/BackEnd/routes/node.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport')
-const Node = require('../schema/NodeSchema');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 const mongoose = require('mongoose');
+const dynamic_schemas = require('mongoose-dynamic-schemas');
+
+const Data = require('../schema/dataschema');
+const Node = require('../schema/NodeSchema');
+
+
 
 router.post('/addNewNode',function(req,res,next){
     let newNode = new Node({
@@ -26,12 +31,30 @@ router.post('/addData',function(req,res,next){
     let data = new Data ({
         Data:req.body.Data
     });
-
 })
 
 router.post('/addNewCollector',function(req,res,next){
 
 });
 
+//testing dynamic schema
+router.post('/addNode', (req, res, next) => {
+    const nodeNumber = req.body.nodeNumber;
+    Data
+        .then(result => {
+            dynamic_schemas.addSchemaField(Data.data, "Node_"+nodeNumber, [{ sensor_1: Number, sensor_2: Number, sensor_3: Number, sensor_4: Number }] )
+        })
+        .catch(err => {
+            if(err){
+                res.status(500).json({
+                    state: false
+                })
+            } else{
+                res.status(200).json({
+                    state: true
+                })
+            }
+        })
+})
 
 module.exports = router;
--- Server/BackEnd/schema/dataschema.js
+++ Server/BackEnd/schema/dataschema.js
@@ -1,3 +1,38 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const config = require('../config/config');
+
+const dataStructure = mongoose.Schema({
+    data: [{
+        Node_1: [{
+            sensor_1: { type: Number },
+            sensor_2: { type: Number },
+            sensor_3: { type: Number },
+            sensor_4: { type: Number }
+        }],
+        Node_2: [{
+            sensor_1: { type: Number },
+            sensor_2: { type: Number },
+            sensor_3: { type: Number },
+            sensor_4: { type: Number }
+        }],
+        Node_3: [{
+            sensor_1: { type: Number },
+            sensor_2: { type: Number },
+            sensor_3: { type: Number },
+            sensor_4: { type: Number }
+        }],
+        Node_4: [{
+            sensor_1: { type: Number },
+            sensor_2: { type: Number },
+            sensor_3: { type: Number },
+            sensor_4: { type: Number }
+        }],
+        Node_5: [{
+            sensor_1: { type: Number },
+            sensor_2: { type: Number },
+            sensor_3: { type: Number },
+            sensor_4: { type: Number }
+        }]
+    }]
+})
+
+module.exports = mongoose.model('Data', dataStructure);
